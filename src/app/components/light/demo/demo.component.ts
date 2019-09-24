5ab0d4214e86ec9bc180285f3ae87e03a039bb98
Veer Panchal <vmpanchal97@gmail.com>
2019-09-24 02:17:41 -0500

    added submit match

--- src/app/components/light/demo/demo.component.ts
+++ src/app/components/light/demo/demo.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+declare var $: any;
+
+interface APIResponse {
+  result: string;
+  msg: string;
+}
+
 
 @Component({
   selector: 'app-demo',
@@ -7,9 +15,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemoComponent implements OnInit {
 
-  constructor() { }
+  team_1; team_2; team_1_score; team_2_score; error = ''; response;
+
+  public apiEndPoint = 'http://pandadevs.org:8080/api/matches?';
+
+  constructor(public http: HttpClient) {}
+
 
   ngOnInit() {
   }
 
+  submit() {
+    this.error = '';
+    const params = new HttpParams().set('team1', this.team_1).set('team2', this.team_2).set('team1score', this.team_1_score).set('team2score', this.team_2_score);
+    const apiURL = this.apiEndPoint + params.toString();
+    console.log(apiURL);
+    this.http.post(apiURL, '').subscribe(response => {
+      this.response = response;
+      return response;
+    }, error => {
+      this.error = 'Sorry, an MailChimp error occurred.';
+    });
+  }
 }
